67f9f5d2088bb2060af0207f9d4c62d8ca4bfc59
Gaurav Chaurasia <gaurav.181cv155@nitk.edu.in>
2020-11-05 16:13:16 +0530

    lab5 work, create server

---
+++ lab5/routes/users.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource');
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact');
+});
+
+router.post('/contact', (req, res, next) => {
+  res.json({
+    body: req.body
+  })
+});
+
+module.exports = router;
